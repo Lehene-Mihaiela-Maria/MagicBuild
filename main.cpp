b404519e32c3a42368274174903c5421a1095369
Mihaiela Lehene <mihaiela_lehene@yahoo.com>
2022-09-09 11:46:28 +0300

    add files

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#define VERSION_APP "1.0.2"
+
+int main(int argc, char** argv){
+	std::cout << "Laitek Magic Build" << std::endl;
+	std::cout << "Version " << VERSION_APP << std::endl;
+	return 0;
+}
